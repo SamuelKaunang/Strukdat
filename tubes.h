4466314cfc823efd78e0818b5f0326b03468f6aa
Sakahere4u_ <148515040+SamuelKaunang@users.noreply.github.com>
2026-03-24 16:13:13 +0700

    Refactor tubes.h header file structure

--- tubes.h
+++ tubes.h
@@ -2,6 +2,7 @@
 #define TUBES_H_INCLUDED
 
 #include <iostream>
+#include <string>
 
 #define Nil NULL
 #define info(P) (P)->info
@@ -14,7 +15,7 @@
 
 using namespace std;
 
-struct model{
+struct model {
     string nama_model;
     int stok_model;
 };
@@ -24,65 +25,41 @@ typedef string infotypeP;
 typedef struct elemen_P *address_P;
 typedef struct elemen_C *address_C;
 
-
-struct elemen_C{
-/* struct ini untuk menyimpan elemen anak dan pointer penunjuk
- elemen tetangganya */
+struct elemen_C {
     infotypeC info;
     address_C next;
 };
 
-struct elemen_P{
-/* struct ini untuk menyimpan elemen induk dan pointer penunjuk
- elemen tetangganya */
+struct elemen_P {
     infotypeP info;
     address_C firstC;
     address_P next;
     address_P prev;
 };
 
-struct ListP{
-/* struct ini digunakan untuk menyimpan list induk itu sendiri */
+struct ListP {
     address_P first;
     address_P last;
 };
 
 void CreateListP(ListP &LP);
-
 address_P alokasiP(infotypeP X);
-
 address_C alokasiC(infotypeC X);
-
 address_P searchP(ListP LP, string namaMerk);
-
 address_C searchC(ListP LP, string namaMerk, string namaModel);
-
 bool findParent(address_P PParent);
-
 bool findChild(address_C PChild);
-
 void insertParent(ListP &LP, address_P PParent);
-
 void insertChild(ListP &LP, address_C PChild, string merk);
-
 void deleteParent(ListP &LP, infotypeP x);
-
 void deleteChild(ListP &LP, string merk, string model);
-
 void deleteAllChild(ListP &LP, address_P PParent);
-
 void showParent(ListP LP);
-
 void showChild(ListP LP);
-
 void updateParent(ListP &LP, address_P PParent, string merkBaru);
-
 void updateChild(ListP &LP, address_C PChild, string merk, string namaModelBaru, int stokBaru);
-
 int countChild(ListP LP);
-
 void dummyData(ListP &LP);
-
 void showMenu();
 
 #endif // TUBES_H_INCLUDED
